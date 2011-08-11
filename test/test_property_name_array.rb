8fcdc543f077f94361e403858b252c8cffb64e15
ppibburr <ppibburr@ppibburr-VirtualBox.(none)>
2011-08-11 19:02:40 -0400

    add wrapping for JSPropertyNameArrayRef and provide some 'native' introspection for JS::Object

--- test/test_property_name_array.rb
+++ test/test_property_name_array.rb
@@ -5,19 +5,26 @@ require File.join(File.dirname(__FILE__),'..','lib','JS')
 ctx = JS::GlobalContext.new(nil)
 obj = JS::Object.new(ctx)
 names = ['foo','bar','baz','moof']
+
 names.each_with_index do |n,i|
   obj[n] = "property #{i}"
 end
 
-p obj.copy_property_names.get_count == names.count
-
-p obj.properties == names
-
-p obj.properties[2] == 'baz'
-p 'baz' == obj.copy_property_names.get_name_at_index(2)
+names << 'myFunc'
 
 obj['myFunc'] = JS::Object.make_function_with_callback(ctx,'myFun') do
   true
 end
 
-p obj.functions == ['myFunc']
+if idx=[
+  obj.copy_property_names.get_count == names.count,
+  obj.properties == names,
+  obj.properties[2] == 'baz',
+  'baz' == obj.copy_property_names.get_name_at_index(2),
+  obj.functions == ['myFunc']
+].index(false) then
+  puts "#{File.basename(__FILE__)} test #{idx} failed"
+  exit(1)
+else
+  puts "#{File.basename(__FILE__)} all passed"
+end
