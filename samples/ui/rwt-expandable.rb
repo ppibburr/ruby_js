30be9af129cb9d40a76b49821e56bff2a74a4e93
ppibburr <ppibburr@ppibburr-HP-G61-Notebook-PC.(none)>
2012-02-07 18:54:30 -0500

    clean up

--- samples/ui/rwt-expandable.rb
+++
@@ -1,8 +0,0 @@
-module Rwt
-  module Expandable
-    def self.extended q
-      raise RuntimeError.new("Object must respond_to? :shade_handle")
-      Rwt::UI::Expandable.new(q.shade_handle,q)
-    end
-  end
-end
