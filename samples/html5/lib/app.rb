df3af0ea6e4f7789cab2bd067a7da27970c54fe4
ppibburr <ppibburr@ppibburr-HP-G61-Notebook-PC.(none)>
2012-04-01 02:36:09 -0400

    put html5 usage with gir_ffi in samples/html5

---
+++ samples/html5/lib/app.rb
@@ -0,0 +1,125 @@
+require File.join(File.dirname(__FILE__),'html5')
+module RubyJS
+  class App
+    HTML = "<html><body></body></html>"
+    attr_reader :shell,:vbox,:view,:top_child
+    def initialize html=HTML,base_uri=nil
+      @shell = Gtk::Window.new :toplevel
+      @vbox = Gtk::VBox.new false,0
+      @top_child = Gtk::VBox.new false,0
+      @view = WebKit::WebView.new
+      
+      @html = html
+      @base_uri = base_uri
+      
+      vbox.pack_start top_child,false,true,0
+      vbox.pack_start view,true,true,0    
+      
+      shell.add vbox
+      
+      shell.set_size_request 400,400
+      shell.signal_connect "delete-event" do
+        on_exit
+      end   
+      
+      view.signal_connect "populate-popup" do |*o|
+        show_popup o[1]
+      end
+      
+      view.signal_connect "load-finished" do |*o|
+        (@_onload_cb||proc do |*o| true  end).call *o
+      end
+    end
+    
+    def show_popup menu
+  	  items = menu.children
+	  case q=items[0].get_label.gsub(/^\_/,'')
+	  when /^Back/
+	    # navigation
+	    navigate_menu(menu)
+	  when /^Cut/
+	    # edit menu
+	    edit_menu(menu)
+  	  when /^Open Link/
+	    # /link menu/
+	    link_menu(menu)
+	  when /^Open \_Image/
+        image_menu(menu)
+      when /^Copy/
+        copy_menu(menu)
+	  else
+	  end
+    end
+    
+    def global_object
+      view.get_main_frame.global_object
+    end
+    
+    def on_exit
+      Gtk.main_quit
+    end
+    
+    def exit
+      on_exit
+    end
+    
+    def display
+      view.load_html_string @html,@base_uri    
+    end
+    
+    def run &b
+      b.call self
+      shell.show_all
+      Gtk.main
+    end
+    
+    def self.run &b
+      Gtk.init []
+      app = self.new
+      app.run &b
+      return app
+    end
+    
+    def navigate_menu menu
+      menu.children.each do |c|
+        c.destroy
+      end
+    end
+    
+    def link_menu menu
+      menu.children.each do |c|
+        label = c.get_label.gsub(/^\_/,'')
+        c.destroy unless label =~ /Copy Link/ || label =~ /Download/
+      end
+      menu.append i=Gtk::MenuItem.new()
+      i.set_label "Open With"
+      i.show
+      i.signal_connect "activate" do
+        global_object.alert("Foo")
+      end    
+    end
+    
+    def edit_menu menu
+    end
+    
+    def copy_menu menu
+    end
+    
+    def image_menu menu
+      menu.children[0].destroy
+    end
+    
+    def onload &b
+      @_onload_cb = b
+    end
+  end
+end
+
+if __FILE__ == $0
+	RubyJS::App.run do |app|
+	  app.onload do |*o|
+		app.global_object.alert("Hello World")
+	  end
+	  app.display
+	end
+end
---
+++ samples/html5/sample.rb
@@ -0,0 +1,19 @@
+require './lib/app'
+class App < RubyJS::App
+  def initialize *o
+    super
+    onload do
+      body = global_object.document.body
+      body.innerText = "Hello World"
+      body.onclick = method(:click)
+    end
+  end
+  
+  def click target,event
+    global_object.alert([target,event])
+  end
+end
+
+App.run do |app| 
+  app.display
+end
